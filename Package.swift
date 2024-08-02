ef3b1b455c59be1256e6bc1a49f8c01787f6cfd4
tumakov <m.tumakov27@gmail.com>
2024-08-02 18:08:42 +0500

    fix package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Obfuscate",
+    platforms: [
+        .iOS(.v12), .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Obfuscate",
             targets: ["Obfuscate"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Obfuscate"),
+            name: "Obfuscate", dependencies: []),
         .testTarget(
             name: "ObfuscateTests",
             dependencies: ["Obfuscate"]),
